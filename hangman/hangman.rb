4f0327c8b7ecdd147f3cd3f42762871d86cc3a0c
VitorB2002 <vitorb.santos2002@gmail.com>
2024-10-15 10:17:17 -0300

    feat: adicionado método para buscar uma palavra aleatória do documento txt

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,7 +1,11 @@
 def random_word
   file = File.open('google-10000-english-no-swears.txt', 'r')
   str_arr = file.read.split("\n")
-  str_arr[rand(str_arr.length)]
+  word = String.new
+  while word.length < 5 || word.length > 12
+    word = str_arr[rand(str_arr.length)]
+  end
+  word
 end
 
 puts random_word
